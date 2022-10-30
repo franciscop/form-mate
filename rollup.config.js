c391dce425096ff670d77198f8ba4d3f128d8eb8
Francisco <public@francisco.io>
2022-10-30 15:47:27 +0900

    Clean up project and dependencies

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,5 @@
-import { terser } from "rollup-plugin-terser";
 import babel from "rollup-plugin-babel";
+import { terser } from "rollup-plugin-terser";
 
 export default {
   input: "src/index.js",
